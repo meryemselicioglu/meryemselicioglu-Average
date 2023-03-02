6f71714023bddb6a2796a1cb435398d89dbeb565
meryemselicioglu <meryemselici2000@gmail.com>
2023-03-02 05:27:20 +0000

    commit changes

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,15 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double sum = 0.0;
+        double avg;
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            sum += nums[i];
+        }
+        
+        avg = sum / nums.length;
+        return avg;
     }
 }
